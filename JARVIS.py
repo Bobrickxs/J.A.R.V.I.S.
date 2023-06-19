ca5a7af1dcbff1a1024b3641b6e34a9961ce791e
Bobrickxs <136356466+Bobrickxs@users.noreply.github.com>
2023-06-19 17:00:22 +0000

    new def talk

--- JARVIS.py
+++ JARVIS.py
@@ -1,1 +1,18 @@
 import speech_recognition as sr
+import sys
+import os
+import pyttsx3
+
+
+engine = pyttsx3.init()
+
+
+
+def talk(words):
+    print(words)
+    engine.say(words)
+    engine.runAndWait()
+    #os.system("say " + words)
+
+
+talk('Hi, can i help you?')
