e7d667ade88fabf840b579a9cdc71326e8bb0120
Bobrickxs <azachemetovsyo@gmail.com>
2023-06-22 19:36:11 +0300

    fixed bug

--- JARVIS.py
+++ JARVIS.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 import speech_recognition as sr
 import sys
 import os
@@ -16,3 +18,31 @@ def talk(words):
 
 
 talk('Hi, can i help you?')
+
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone as source:
+        print('Say')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        task = r.recognize_google(audio,language='ua-UA').lower()
+        print(task)
+    except sr.UnknownValueError:
+        talk('Я вас не зрозумів')
+        task= command()
+
+
+    return task
+
+
+def make_something(task):
+    if 'Відкрити сайт' in task:
+        talk('Відкриваю')
+        url = 'https://it-univer.thecabinet.io/profile/account_myorders/ '
+        webbrowser.open(url)
+
+while True:
+    make_something()
